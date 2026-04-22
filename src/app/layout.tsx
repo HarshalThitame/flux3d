8c30bd435db89e209855ad75d44838c08195d3ac
Harshal Thitame <hthitame@gmail.com>
2026-04-23 00:16:27 +0530

    fix: add default export to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,3 +2,14 @@ import {DM_Sans, Syne} from 'next/font/google'
 
 const syne = Syne({subsets: ['latin'], variable: '--font-syne'})
 const dmSans = DM_Sans({subsets: ['latin'], variable: '--font-dm'})
+export default function RootLayout({
+                                       children,
+                                   }: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+        <body>{children}</body>
+        </html>
+    )
+}
