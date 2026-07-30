9c83abaa8b25224f64c12871e5840196434cd8cd
rutikthitame <thitamerutik3736@gmail.com>
2026-07-31 03:00:53 +0530

    refactor: use next/script for Meta pixel injection per standard placement

--- src/components/MetaPixel.tsx
+++ src/components/MetaPixel.tsx
@@ -1,52 +1,37 @@
-'use client'
-
-import { useEffect, useRef } from 'react'
-
-declare global {
-  interface Window {
-    fbq?: (command: string, event: string, data?: Record<string, unknown>, options?: Record<string, unknown>) => void
-    _fbq?: unknown
-  }
-}
+import Script from 'next/script'
 
 const PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID || ''
 
 export default function MetaPixel() {
-  const loaded = useRef(false)
-
-  useEffect(() => {
-    if (loaded.current) return
-    loaded.current = true
-    if (!PIXEL_ID) return
-
-    const n = window as unknown as Record<string, unknown>
-    if (n.fbq) return
-
-    const fbq = (...args: unknown[]) => {
-      const q = (fbq as unknown as { queue?: unknown[] }).queue ??= []
-      q.push(args)
-    }
-    n.fbq = fbq as typeof window.fbq
-    if (!n._fbq) n._fbq = fbq
-
-    const script = document.createElement('script')
-    script.async = true
-    script.src = 'https://connect.facebook.net/en_US/fbevents.js'
-    document.head.appendChild(script)
-
-    fbq('init', PIXEL_ID)
-    fbq('track', 'PageView')
-  }, [])
+  if (!PIXEL_ID) return null
 
   return (
-    <noscript>
-      <img
-        height={1}
-        width={1}
-        style={{ display: 'none' }}
-        alt=""
-        src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
+    <>
+      <Script
+        id="meta-pixel-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s){
+              if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];
+              t=b.createElement(e);t.async=!0;t.src=v;
+              s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)
+            }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${PIXEL_ID}');
+            fbq('track', 'PageView');
+          `,
+        }}
       />
-    </noscript>
+      <noscript>
+        <img
+          height={1}
+          width={1}
+          style={{ display: 'none' }}
+          alt=""
+          src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
+        />
+      </noscript>
+    </>
   )
 }
